c5af22410fb4e9e76a0512aadd48cb51250d3b45
nic-gitlab <74934107+nic-gitlab@users.noreply.github.com>
2021-06-24 12:14:48 -0700

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Nic Rowe on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
